d78fd54eee8e6816cb4c142152a3246f9d58252c
yff <2408216493@qq.com>
2020-01-20 19:38:23 +0800

    up

--- node2/setHeader.js
+++ node2/setHeader.js
@@ -1,23 +1,29 @@
 const http = require("http");
+const queryString = require("querystring");
 const server = http.createServer((req, res) => {
     const reqN = req;
     const { url, method } = req;
     const path = url.split("?")[0];
-    const query = url.split("?")[1];
-    res.setHeader("Content-type", "application/json");
+    const query = queryString.parse(url.split("?")[1]);
+    res.setHeader("Content-type", "application/json"); //设置返回格式,针对字符串
+    let resData = {
+        url,
+        method,
+        query,
+        path
+    };
     if (method === "GET") {
-        res.end({
-            url,
-            method,
-            query,
-            path
-        });
+        res.end(JSON.stringify(resData));
     } else {
         let postData = "";
         reqN.on("data", chunck => {
             postData += chunck.toString();
         });
-        reqN.end(() => {});
+        reqN.on("end", () => {
+            resData.postData = postData;
+            res.end(JSON.stringify(resData));
+        });
     }
 });
 server.listen(3300);
+console.log(33);
